2e616db81e6d23c7d001d1ed9c430f779b1b13c0
Armin Hueneburg <hueneburg.armin@gmail.com>
2017-09-30 19:10:06 +0200

    blargh
    Signed-off-by: Armin Hueneburg <hueneburg.armin@gmail.com>

--- update.sh
+++ update.sh
@@ -51,6 +51,8 @@ main() {
 	then incremental_deploy
 	else initial_deploy
 	fi
+
+	update_repos
 }
 
 remove_files(){
@@ -125,6 +127,14 @@ disable_expanded_output() {
 		set +o xtrace
 		set -o verbose
 	fi
+}
+
+# run script to update repos
+update_repos () {
+  which node
+  which nodejs
+  npm install github-api simple-git
+  node update-repos.js ${GITHUB_TOKEN}
 }
 
 main
